d958733b64237be4947032c4d47ad099869a4159
Kyle Sunden <git@ksunden.space>
2020-09-28 14:33:18 -0500

    initial fill of daemon implementation

--- yaqd_attune/_attune.py
+++ yaqd_attune/_attune.py
@@ -11,21 +11,66 @@ class Attune(ContinuousHardware):
 
     def __init__(self, name, config, config_filepath):
         super().__init__(name, config, config_filepath)
-        # Perform any unique initialization
-
+        self._instrument = attune.load(name)
+        self._setables = {k: yaqc.Client(v) for k, v in config["setables"].items()}
 
     def _set_position(self, position):
-        ...
+        self.set_position_except(position)
 
+    def set_position_except(self, position, exceptions=None):
+        self._busy = True
+        self._state["destination"] = position
+        if exceptions is None:
+            exceptions = []
+        for name, set_pos in self._instrument(position, self._state["arrangement"]).items():
+            if name in exceptions:
+                continue
+            self._setables[name].set_position(set_pos)
+        self._state["position"] = position
+
+    def get_instrument(self):
+        return self._instrument.as_dict()
+
+    def set_instrument(self, instrument):
+        self._instrument = attune.Instrument(**instrument)
+        self._instrument.store()
+        self.shutdown(restart=True)
+
+    def get_arrangement(self):
+        return self._state["arrangement"]
+
+    def set_arrangement(self, arrangement):
+        self._state["arrangement"] = arrangement
+
+    def get_all_arrangements(self):
+        return list(instrument.arrangements.keys())
+
+    def get_setable_yaq_params(self):
+        return self.config["setables"]
+
+    def get_setable_names(self):
+        return list(self._setables.keys())
+
+    def set_setable_postitions(self, setables):
+        for name, set_pos in setables.items():
+            self._setables[name].set_position(set_pos)
+
+    def get_setable_postitions(self):
+        return {k: v.get_position() for k, v in self._setables.items()}
+
+    def home_setables(self, setables):
+        for name in setables:
+            if hasattr(self._setables[name], "home"):
+                self._setables[name].home()
+
+    def home(self):
+        self.home_setables(self._setables.keys())
 
     async def update_state(self):
         """Continually monitor and update the current daemon state."""
-        # If there is no state to monitor continuously, delete this function
         while True:
-            # Perform any updates to internal state
-            self._busy = False
-            # There must be at least one `await` in this loop
-            # This one waits for something to trigger the "busy" state
-            # (Setting `self._busy = True)
-            # Otherwise, you can simply `await asyncio.sleep(0.01)`
-            await self._busy_sig.wait()
+            self._busy = any([sa.busy() for sa in self._setables.values()])
+            if self._busy:
+                await asyncio.sleep(0.1)
+            else:
+                await self._busy_sig.wait()
