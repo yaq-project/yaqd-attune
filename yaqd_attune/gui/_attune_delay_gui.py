1a3003e1cd41252ddbfeeec3433bc2e20be155c8
Kyle Sunden <git@ksunden.space>
2021-12-09 21:53:18 +0000

    Attune opa and delay qtpy main widgets with updated qtypes

--- yaqd_attune/gui/_attune_delay_gui.py
+++ yaqd_attune/gui/_attune_delay_gui.py
@@ -1,14 +1,160 @@
+from functools import partial
+
 from qtpy import QtWidgets, QtCore  # type: ignore
+import qtypes  # type: ignore
 
 import yaqc_qtpy  # type: ignore
+from yaqc_qtpy import _plot, qtype_items  # noqa
+import WrightTools as wt  # type: ignore
+import attune  # type: ignore
+
+import yaq_traits  # type: ignore
 
 
-class AttuneDelayGUI(QtWidgets.QWidget):
-    def __init__(self, client: yaqc_qtpy.QClient):
+class AttuneDelayGUI(QtWidgets.QSplitter):
+    def __init__(self, qclient: yaqc_qtpy.QClient):
         super().__init__()
+        self.units = "nm"  # TODO synchronous getter calls at startup?
+        self.arr = None
+        self.plot_widget = yaqc_qtpy._plot.Plot1D(yAutoRange=True)
+        # self.plot_v_line = self.plot_widget.add_infinite_line(angle=90, hide=False)
+        self.arrangement_enum = qtypes.Enum("Arrangement")
+        self.arrangement_enum.updated.connect(self.on_arrangement_updated)
+        self.tune_enum = qtypes.Enum("Tune")
+        self.instrument_item = qtypes.Null("instrument")
+        allowed_values = ["wn"] + list(wt.units.get_valid_conversions("wn"))
+        self.plot_units = qtypes.Enum(
+            "Units", value={"value": self.units, "allowed": allowed_values}
+        )
+        self.qclient = qclient
+        self.qclient.get_instrument.finished.connect(self.on_get_instrument)
+        self.qclient.get_instrument()
+        self.qclient.get_control_active.finished.connect(self.on_get_control_active)
         self._create_main_frame()
 
+        self.poll = QtCore.QTimer()
+        self.poll.timeout.connect(self.qclient.get_control_active)
+        self.poll.start(20)
+
     def _create_main_frame(self):
-        hbox = QtWidgets.QHBoxLayout()
-        hbox.addWidget(QtWidgets.QLabel("this widget provided by yaqd-attune"))
-        self.setLayout(hbox)
+        # container widget
+        display_container_widget = QtWidgets.QWidget()
+        display_container_widget.setLayout(QtWidgets.QVBoxLayout())
+        display_layout = display_container_widget.layout()
+        display_layout.setContentsMargins(0, 0, 0, 0)
+        self.addWidget(display_container_widget)
+        # plot
+        self.plot_widget.plot_object.setMouseEnabled(False, False)
+        self.plot_curve = self.plot_widget.add_scatter()
+        # self.plot_h_line = self.plot_widget.add_infinite_line(angle=0, hide=False)
+        display_layout.addWidget(self.plot_widget)
+
+        # right hand tree
+        self._tree_widget = qtypes.TreeWidget(width=500)
+
+        # plot control
+        display_item = qtypes.Null("Display")
+        self._tree_widget.append(display_item)
+        display_item.append(self.arrangement_enum)
+        self.tune_enum.updated.connect(self.update_plot)
+        display_item.append(self.tune_enum)
+        self.plot_units.updated.connect(self.update_plot)
+        display_item.append(self.plot_units)
+        display_item.setExpanded(True)
+
+        # id
+        id_item = qtypes.Null("id")
+        self._tree_widget.append(id_item)
+        for key, value in self.qclient.id().items():
+            id_item.append(qtypes.String(label=key, disabled=True, value={"value": value}))
+        id_item.setExpanded(True)
+
+        # traits
+        traits_item = qtypes.Null("traits")
+        self._tree_widget.append(traits_item)
+        for trait in yaq_traits.__traits__.traits.keys():
+            traits_item.append(
+                qtypes.Bool(
+                    label=trait, disabled=True, value={"value": trait in self.qclient.traits}
+                )
+            )
+
+        # properties
+        properties_item = qtypes.Null("properties")
+        self._tree_widget.append(properties_item)
+        qtype_items.append_properties(self.qclient, properties_item)
+        properties_item.setExpanded(True)
+
+        # is-homeable
+        if "is-homeable" in self.qclient.traits:
+
+            def on_clicked(_, qclient):
+                qclient.home()
+
+            home_button = qtypes.Button("is-homeable", value={"text": "home"})
+            self._tree_widget.append(home_button)
+            home_button.updated.connect(partial(on_clicked, qclient=self.qclient))
+
+        # instrument preview
+        self._tree_widget.append(self.instrument_item)
+        self.instrument_item.setExpanded(True)
+
+        self._tree_widget.resizeColumnToContents(0)
+        self.addWidget(self._tree_widget)
+
+        self.update()
+        self.update_plot()
+
+    def update_plot(self):
+        if not hasattr(self, "instrument"):
+            return
+        arr = self.arrangement_enum.get_value()
+        if arr is None:
+            return
+        motor_name = self.tune_enum.get_value()
+        tune = self.instrument.arrangements[arr][motor_name]
+        if not isinstance(tune, attune.Tune):
+            return
+        # units
+        units = self.plot_units.get_value()
+        # xi
+        colors = tune.independent
+        xi = wt.units.converter(colors, tune.ind_units, units)
+        # yi
+        yi = tune.dependent
+        self.plot_widget.set_labels(xlabel=units, ylabel=motor_name)
+        self.plot_curve.clear()
+        try:
+            self.plot_curve.setData(xi, yi)
+        except ValueError:
+            pass
+        self.plot_widget.graphics_layout.update()
+        self.qclient.get_position()
+        self.update()
+
+    def on_get_instrument(self, json):
+        self.instrument = attune.Instrument(**json)
+        self.arrangement_enum.set({"allowed": list(self.instrument.arrangements.keys())})
+        self.use_bools = {}
+        # TODO empty instrument item
+        for name, arr in self.instrument.arrangements.items():
+            arr_item = qtypes.Bool(name)
+            self.use_bools[name] = arr_item
+
+            def update_use(val, name=name):
+                self.qclient.set_control_active(name, val["value"])
+
+            arr_item.edited.connect(update_use)
+            self.instrument_item.append(arr_item)
+            for tune in arr.tunes.keys():
+                arr_item.append(qtypes.Null(tune))
+        self.update_plot()
+
+    def on_arrangement_updated(self, arr):
+        arr = arr["value"]
+        self.tune_enum.set({"allowed": list(self.instrument[arr].keys())})
+        self.update_plot()
+
+    def on_get_control_active(self, active):
+        for name, use in self.use_bools.items():
+            use.set({"value": active.get(name, False)})
--- yaqd_attune/gui/_attune_gui.py
+++ yaqd_attune/gui/_attune_gui.py
@@ -1,99 +1,115 @@
+from functools import partial
+
 from qtpy import QtWidgets, QtCore  # type: ignore
 import qtypes  # type: ignore
 
 import yaqc_qtpy  # type: ignore
-from yaqc_qtpy import _plot  # noqa
+from yaqc_qtpy import _plot, qtype_items  # noqa
 import WrightTools as wt  # type: ignore
 import attune  # type: ignore
 
+import yaq_traits  # type: ignore
 
-class AttuneGUI(QtWidgets.QWidget):
+
+class AttuneGUI(QtWidgets.QSplitter):
     def __init__(self, qclient: yaqc_qtpy.QClient):
         super().__init__()
         self.units = "nm"  # TODO synchronous getter calls at startup?
-        self.plot_widget = yaqc_qtpy._plot.Plot1D()
+        self.arr = None
+        self.plot_widget = yaqc_qtpy._plot.Plot1D(yAutoRange=True)
         self.plot_v_line = self.plot_widget.add_infinite_line(angle=90, hide=False)
-        self.arrangement_enum = qtypes.Enum()
-        self.tune_enum = qtypes.Enum()
+        self.tune_enum = qtypes.Enum("Tune")
+        self.instrument_item = qtypes.Null("instrument")
         allowed_values = ["wn"] + list(wt.units.get_valid_conversions("wn"))
-        self.plot_units = qtypes.Enum(initial_value=self.units, allowed_values=allowed_values)
-        self.low_energy_limit_display = qtypes.Number(units=self.units, display=True)
-        self.high_energy_limit_display = qtypes.Number(units=self.units, display=True)
+        self.plot_units = qtypes.Enum(
+            "Units", value={"value": self.units, "allowed": allowed_values}
+        )
         self.qclient = qclient
         self.qclient.get_instrument.finished.connect(self.on_get_instrument)
         self.qclient.get_instrument()
-        self.qclient.get_limits.finished.connect(self.on_get_limits)
         self.qclient.get_position.finished.connect(self.on_get_position)
-        self.qclient.get_limits()
+        self.qclient.get_arrangement.finished.connect(self.on_arrangement_updated)
         self._create_main_frame()
 
     def _create_main_frame(self):
-        hbox = QtWidgets.QHBoxLayout()
-        self.setLayout(hbox)
         # container widget
         display_container_widget = QtWidgets.QWidget()
         display_container_widget.setLayout(QtWidgets.QVBoxLayout())
         display_layout = display_container_widget.layout()
         display_layout.setContentsMargins(0, 0, 0, 0)
-        hbox.addWidget(display_container_widget)
+        self.addWidget(display_container_widget)
         # plot
         self.plot_widget.plot_object.setMouseEnabled(False, False)
         self.plot_curve = self.plot_widget.add_scatter()
         # self.plot_h_line = self.plot_widget.add_infinite_line(angle=0, hide=False)
         display_layout.addWidget(self.plot_widget)
-        # vertical line
-        line = qtypes.widgets.Line("V")
-        hbox.addWidget(line)
-        # container widget / scroll area
-        settings_container_widget = QtWidgets.QWidget()
-        settings_scroll_area = qtypes.widgets.ScrollArea()
-        settings_scroll_area.setWidget(settings_container_widget)
-        settings_scroll_area.setMinimumWidth(500)
-        settings_scroll_area.setMaximumWidth(500)
-        settings_container_widget.setLayout(QtWidgets.QVBoxLayout())
-        settings_layout = settings_container_widget.layout()
-        settings_layout.setContentsMargins(5, 5, 5, 5)
 
-        hbox.addWidget(settings_scroll_area)
-        # opa properties
-        input_table = qtypes.widgets.InputTable()
-        settings_layout.addWidget(input_table)
+        # right hand tree
+        self._tree_widget = qtypes.TreeWidget(width=500)
+
         # plot control
-        input_table = qtypes.widgets.InputTable()
-        input_table.append(None, "Display")
+        display_item = qtypes.Null("Display")
+        self._tree_widget.append(display_item)
         self.tune_enum.updated.connect(self.update_plot)
-        input_table.append(self.tune_enum, "Tune")
+        display_item.append(self.tune_enum)
         self.plot_units.updated.connect(self.update_plot)
-        input_table.append(self.plot_units, "Units")
-        settings_layout.addWidget(input_table)
-        # curves
-        input_table = qtypes.widgets.InputTable()
-        # input_table.append(None, "Curves")
-        self.arrangement_enum.updated.connect(self.on_arrangement_updated)
-        input_table.append(self.arrangement_enum, "Arrangement")
-        # limits
-        input_table.append(self.low_energy_limit_display, "Low Limit")
-        input_table.append(self.high_energy_limit_display, "High Limit")
-        settings_layout.addWidget(input_table)
-        self.home_all_button = qtypes.widgets.PushButton("HOME ALL")
-        settings_layout.addWidget(self.home_all_button)
-        self.home_all_button.clicked.connect(self.on_home_all)
-        # stretch
-        settings_layout.addStretch(1)
-        # finish
+        display_item.append(self.plot_units)
+        display_item.setExpanded(True)
+
+        # id
+        id_item = qtypes.Null("id")
+        self._tree_widget.append(id_item)
+        for key, value in self.qclient.id().items():
+            id_item.append(qtypes.String(label=key, disabled=True, value={"value": value}))
+        id_item.setExpanded(True)
+
+        # traits
+        traits_item = qtypes.Null("traits")
+        self._tree_widget.append(traits_item)
+        for trait in yaq_traits.__traits__.traits.keys():
+            traits_item.append(
+                qtypes.Bool(
+                    label=trait, disabled=True, value={"value": trait in self.qclient.traits}
+                )
+            )
+
+        # properties
+        properties_item = qtypes.Null("properties")
+        self._tree_widget.append(properties_item)
+        qtype_items.append_properties(self.qclient, properties_item)
+        properties_item.setExpanded(True)
+
+        # is-homeable
+        if "is-homeable" in self.qclient.traits:
+
+            def on_clicked(_, qclient):
+                qclient.home()
+
+            home_button = qtypes.Button("is-homeable", value={"text": "home"})
+            self._tree_widget.append(home_button)
+            home_button.updated.connect(partial(on_clicked, qclient=self.qclient))
+
+        # instrument preview
+        self._tree_widget.append(self.instrument_item)
+
+        self._tree_widget.resizeColumnToContents(0)
+        self.addWidget(self._tree_widget)
+
         self.update()
         self.update_plot()
 
     def update_plot(self):
         if not hasattr(self, "instrument"):
             return
-        arr = self.arrangement_enum.get()
-        motor_name = self.tune_enum.get()
+        arr = self.arr
+        if arr is None:
+            return
+        motor_name = self.tune_enum.get_value()
         tune = self.instrument.arrangements[arr][motor_name]
         if not isinstance(tune, attune.Tune):
             return
         # units
-        units = self.plot_units.get()
+        units = self.plot_units.get_value()
         # xi
         colors = tune.independent
         xi = wt.units.converter(colors, tune.ind_units, units)
@@ -106,28 +122,25 @@ class AttuneGUI(QtWidgets.QWidget):
         except ValueError:
             pass
         self.plot_widget.graphics_layout.update()
+        self.qclient.get_position()
         self.update()
 
     def on_get_instrument(self, json):
         self.instrument = attune.Instrument(**json)
-        self.arrangement_enum.set_allowed_values(self.instrument.arrangements.keys())
-        self.tune_enum.set_allowed_values(self.instrument[self.arrangement_enum.get()].keys())
         self.update_plot()
+        # TODO empty instrument item
+        for name, arr in self.instrument.arrangements.items():
+            arr_item = qtypes.String(
+                name, value={"value": f"{arr.ind_min:0.3f} - {arr.ind_max:0.3f} nm"}, disabled=True
+            )
+            self.instrument_item.append(arr_item)
+            for tune in arr.tunes.keys():
+                arr_item.append(qtypes.Null(tune))
 
-    def on_home_all(self):
-        self.qclient.home()
-
-    def on_get_limits(self, limits):
-        low_energy_limit, high_energy_limit = limits
-        self.low_energy_limit_display.set(low_energy_limit, self.units)
-        self.high_energy_limit_display.set(high_energy_limit, self.units)
-
-    def on_arrangement_updated(self):
-        arr = self.arrangement_enum.get()
-        self.qclient.set_arrangement(arr)
-        self.qclient.get_limits()
-        self.tune_enum.set_allowed_values(self.instrument[arr].keys())
+    def on_arrangement_updated(self, arr):
+        self.arr = arr
+        self.tune_enum.set({"allowed": list(self.instrument[arr].keys())})
         self.update_plot()
 
     def on_get_position(self, pos):
-        self.plot_v_line.setValue(wt.units.convert(pos, self.units, self.plot_units.get()))
+        self.plot_v_line.setValue(wt.units.convert(pos, self.units, self.plot_units.get_value()))
